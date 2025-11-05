be9756b08a0bed289a60d47ce5023c1e044d2ad1
Cyker002 <caykechen3@gmail.com>
2025-11-04 21:45:12 -0300

    Screen Size

--- Game/src/main/java/br/edu/ifsp/UI/Colecao.java
+++ Game/src/main/java/br/edu/ifsp/UI/Colecao.java
@@ -61,7 +61,7 @@ public class Colecao extends Application {
         // FIM CONTAINER
         container.getChildren().addAll( header, body );
 
-        Scene cena = new Scene( container, 1820, 700
+        Scene cena = new Scene( container, 1600, 700
         );
 
         stage.setScene( cena );
